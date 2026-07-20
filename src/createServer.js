fe6d5f684377eea1315cf71fd33782cbd0c2437d
Iago Rodrigues <yagomello16@gmail.com>
2026-07-20 00:24:42 -0300

    chore: abstract errors and add global error handler

--- src/createServer.js
+++ src/createServer.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const usersRoutes = require('./routes/users.route');
 const expensesRoutes = require('./routes/expenses.route');
+const { AppError } = require('./errors/app.errors');
 
 function createServer() {
   const app = express();
@@ -14,6 +15,33 @@ function createServer() {
 
   app.use('/users', usersRoutes);
   app.use('/expenses', expensesRoutes);
+
+  // eslint-disable-next-line no-unused-vars
+  app.use(
+    /**
+     * @param {Error} err
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     * @param {import('express').NextFunction} next
+     */
+    (err, req, res, next) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+
+      if (err instanceof AppError) {
+        res.status(err.statusCode).json({ error: err.message });
+
+        return;
+      }
+
+      const message =
+        process.env.NODE_ENV === 'production'
+          ? 'Internal Server Error'
+          : err.message;
+
+      res.status(500).json({ error: message });
+    },
+  );
 
   return app;
 }
