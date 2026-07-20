d7927636856c1ee34cb2a3ef4ece34a094a143d4
Iago Rodrigues <yagomello16@gmail.com>
2026-07-20 00:25:38 -0300

    chore: add query wrapper to automatically throw on data error

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const pg = require('pg');
 const dotenv = require('dotenv');
+const { DataAccessError } = require('../errors/app.errors');
 
 dotenv.config();
 
@@ -14,8 +17,23 @@ const db = new pg.Pool({
   database: process.env.DB_NAME,
 });
 
-db.connect();
+/**
+ * @template {import('pg').QueryResultRow} T
+ * @param {string} sql
+ * @param {unknown[]} [params]
+ * @returns {Promise<import('pg').QueryResult<T>>}
+ */
+const query = async (sql, params = []) => {
+  try {
+    return /** @type {import('pg').QueryResult<T>} */ (
+      await db.query(sql, params)
+    );
+  } catch (error) {
+    throw new DataAccessError(`Failed to run query`, { cause: error });
+  }
+};
 
 module.exports = {
   db,
+  query,
 };
